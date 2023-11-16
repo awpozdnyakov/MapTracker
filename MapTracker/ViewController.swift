8c01b8c70d2dd77ee3be7d85502443c0e56897ff
awpozdnyakov <102249897+awpozdnyakov@users.noreply.github.com>
2023-11-17 05:13:06 +0700

    [IOS-001] Set the project

---
+++ MapTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapTracker
+//
+//  Created by Andrey Pozdnyakov on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
